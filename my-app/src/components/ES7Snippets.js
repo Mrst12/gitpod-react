b5312b0a1a75102a3535095ea92f46696122978c
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-13 18:45:39 +0000

    add snippets info to file for future reference

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,54 @@
+// rfce creates and automatically export functional component
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rafce creates and exports as an arrow function
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rce create and export class function
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+// rconst creates the constructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// clg logs to the console
+// console.log(first)
+
+
+// clo log an object along with its name as a string
+// console.log('first', first)
