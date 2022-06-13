aed8cbc364aafbc99ab8156f44c53188783cf307
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-13 20:15:14 +0000

    Add event binding for information purposes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallb
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 import EventsFunctional from './components/EventsFunctional';
 import EventsClass from './components/EventsClass';
+import EventBinding from './components/EventBinding';
 
 function App() {
   return (
@@ -15,9 +16,10 @@ function App() {
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
       <StatefulGreeting greeting="I'm a stateful class component!" />
   <StatefulGreetingWithCallback />
-  <StatefulGreetingWithPrevState />*/}
+  <StatefulGreetingWithPrevState />
       <EventsFunctional />
-      <EventsClass />
+  <EventsClass />*/}
+      <EventBinding />
     </div>
   );
 }
