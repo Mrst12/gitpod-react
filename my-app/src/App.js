774f5272b99012c4116282dd404bff8d5d42d065
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-23 12:46:53 +0000

    add learning content for fetching api data using axios in a class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,6 +24,7 @@ import UseStateWithArrays from './components/UseStateWithArrays';
 import UseStateWithObjects from './components/UseStateWithObjects';
 import UseEffectCounter from './components/UseEffectCounter';
 import UseEffectCounterContainer from './components/UseEffectCounterContainer';
+import HTTPRequests from './components/HTTPRequests';
 
 function App() {
   return (
@@ -51,7 +52,8 @@ function App() {
   <UseStateWithArrays />
   <UseStateWithObjects />
   <UseEffectCounter />*/}
-      <UseEffectCounterContainer />
+      {/*<UseEffectCounterContainer />*/}
+      <HTTPRequests />
     </div>
   );
 }
