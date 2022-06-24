9bae3526ea4373bd0788a2b49752c4d27049d145
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-24 09:28:50 +0000

    add hooks to functional component to make it a class functionality with both post and get requests

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,6 +26,7 @@ import UseEffectCounter from './components/UseEffectCounter';
 import UseEffectCounterContainer from './components/UseEffectCounterContainer';
 import HTTPRequests from './components/HTTPRequests';
 import HTTPPost from './components/HTTPPost';
+import HTTPHooks from './components/HTTPHooks';
 
 function App() {
   return (
@@ -55,7 +56,8 @@ function App() {
   {/*<UseEffectCounter />*/}
       {/*<UseEffectCounterContainer />*/}
       {/*<HTTPRequests />*/}
-      <HTTPPost />
+      {/*<HTTPPost />*/}
+      <HTTPHooks />
     </div>
   );
 }
